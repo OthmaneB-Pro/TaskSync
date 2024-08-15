baf57733280f69e5a1988171ffb42acc19023b2d
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-15 12:53:38 +0200

    🚧 Login : LoginForm > Add text and button

---
+++ src/components/pages/login/LoginForm.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export default function LoginForm() {
+  return (
+    <LoginFormStyled>
+      <div>
+        <h1>Bienvenue sur TaskSync !</h1>
+        <p>
+          Votre solution tout-en-un pour organiser et synchroniser vos tâches.
+          Gérez facilement vos projets, suivez vos progrès et collaborez en
+          temps réel, le tout depuis un seul endroit. Restez productif et
+          simplifiez votre quotidien avec TaskSync !
+        </p>
+        <p>
+Connectez-vous ! Pas encore de compte ? Inscrivez-vous
+</p>
+      </div>
+      <div>
+        <button>S'inscrire</button>
+        <button>Se connecter</button>
+      </div>
+    </LoginFormStyled>
+  );
+}
+
+const LoginFormStyled = styled.div`
+  
+  h1{
+    
+    margin-bottom: 35px;
+  }
+  p{
+    text-align: center;
+    width: 500px;
+  }
+
+  button {
+    padding: 15px;
+    width: 200px;
+    background-color: #3755167d;
+    border: 1px solid white;
+    border-radius: 5px;
+    color: white;
+    &:hover {
+      background-color: white;
+      color: #3755167d;
+      cursor: pointer;
+      border-color: #3755167d;
+      transition: 0.3s;
+    }
+  }
+`;
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,23 +1,27 @@
 import styled from "styled-components"
+import LoginForm from "./LoginForm"
 
 export default function LoginPage() {
   return (
     <LoginPageStyled>
-       <div className="container"> TaskSync</div>
+       <div className="container">
+         <LoginForm/>
+        </div>
        </LoginPageStyled>
   )
 }
 
 const LoginPageStyled = styled.div`
-    background-color: red;
+    background-color: #528a177e;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
     .container{
-        background-color: blue;
+        background-color: white;
         width: 1400px;
         height: 600px;
-        
+        border-radius: 15px;
+        border: 1px solid black;
     }
 `
