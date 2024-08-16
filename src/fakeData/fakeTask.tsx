348ee853e6be3c27560b75aa56ca5f0b026b31d7
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-16 16:26:33 +0200

    ✨ Interface : FakeTasks> Add fake data to test

---
+++ src/fakeData/fakeTask.tsx
@@ -0,0 +1,39 @@
+export const tasks = [
+    {
+      id: 1,
+      title: "Créer la page d'accueil",
+      description: "Finaliser la conception de la page d'accueil avec toutes les sections.",
+      dueDate: "20/07/2024",
+      assignee: "Assia",
+      tags: ["Urgent"],
+      status: "To Do",
+    },
+    {
+      id: 2,
+      title: "API pour authentification",
+      description: "Développer l'API pour la gestion des tokens JWT.",
+      dueDate: "25/08/2024",
+      assignee: "Soumeya",
+      tags: ["Pas urgent"],
+      status: "To Do",
+    },
+    {
+        id: 1,
+        title: "Créer la page d'accueil",
+        description: "Finaliser la conception de la page d'accueil avec toutes les sections.",
+        dueDate: "20/07/2024",
+        assignee: "Assia",
+        tags: ["Urgent"],
+        status: "To Do",
+      },
+      {
+        id: 2,
+        title: "API pour authentification",
+        description: "Développer l'API pour la gestion des tokens JWT.",
+        dueDate: "25/08/2024",
+        assignee: "Soumeya",
+        tags: ["Pas urgent"],
+        status: "To Do",
+      },
+  ];
+  
